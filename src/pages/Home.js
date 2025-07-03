4f6108824b2b55a9f863dd4804de8a22dcc84471
vemiotics <jill.johnson1337@gmail.com>
2025-07-02 22:07:22 -0500

    added styling to sign in button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,24 @@
+import Nav from '../components/Nav';
+
 const Home = () => {
+    
+    const authToken = false
+
+    const handleClick = () => {
+        console.log('clicked')
+    }
+
     return (
-        <div>Home</div>
+        <>
+        <Nav/>
+        <div className = "home">
+            <h1>Swipe Right</h1>
+            <button className = "primary-button" onClick={handleClick}>
+                {authToken ? 'Signout' : 'Create Account'}
+            </button>
+
+        </div>
+        </>
     )
 }
 export default Home
