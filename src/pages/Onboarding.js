e5ded0bc41982048157f86b629922a276d6c8574
vemiotics <jill.johnson1337@gmail.com>
2025-07-05 16:06:38 -0500

    added onboarding form with functionality to handle changes and take in form data as variables

--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -3,13 +3,41 @@ import Nav from '../components/Nav';
 
 const Onboarding = () => {
     
+    const [formData, setFormData] = useState({
+        user_id: "",
+        artist_name: "",
+        dob_day: "",
+        dob_month: "",
+        dob_year: "",
+        show_gender: false,
+        gender_identity: "man",
+        event_interest: [],
+        email: "",
+        url: "",
+        about: "",
+        matches: []
+    })
+
     const handleSubmit = () => {
         console.log('submitted');
     }
 
-    const handleChange = () => {
-        console.log('changed');
+    const handleChange = (e) => {
+        console.log('e', e);
+        const value = e.target.value;
+        const name = e.target.name;
+        console.log('value: ' + value + ' name: ' + name);
+        
+        setFormData((prevState) => ({
+            ...prevState,
+            [name]: value
+        }))
+
+        console.log('formData ', formData);
+   
     }
+
+
     
     return (
         <>
@@ -27,81 +55,82 @@ const Onboarding = () => {
                         <input
                             id="artist_name"
                             type="text"
+                            name="artist_name"
                             placeholder="Artist Name"
                             required={true}
-                            value={""}
+                            value={formData.artist_name}
                             onChange={handleChange}
                         />
 
                         <label>Birthday</label>
                         <div className="multiple-input-container">
-                        <input
-                            id="dob_day"
-                            type="number"
-                            name="dob_day"
-                            placeholder="DD"
-                            required={true}
-                            value={""}
-                            onChange={handleChange}
-                        />
                             <input
-                            id="dob_month"
-                            type="number"
-                            name="dob_month"
-                            placeholder="MM"
-                            required={true}
-                            value={""}
-                            onChange={handleChange}
-                        />
-                        <input
-                            id="dob_year"
-                            type="number"
-                            name="dob_year"
-                            placeholder="YYYY"
-                            required={true}
-                            value={""}
-                            onChange={handleChange}
-                        />
+                                id="dob_day"
+                                type="number"
+                                name="dob_day"
+                                placeholder="DD"
+                                required={true}
+                                value={formData.dob_day}
+                                onChange={handleChange}
+                            />
+                                <input
+                                id="dob_month"
+                                type="number"
+                                name="dob_month"
+                                placeholder="MM"
+                                required={true}
+                                value={formData.dob_month}
+                                onChange={handleChange}
+                            />
+                            <input
+                                id="dob_year"
+                                type="number"
+                                name="dob_year"
+                                placeholder="YYYY"
+                                required={true}
+                                value={formData.dob_year}
+                                onChange={handleChange}
+                            />
                         </div>
 
                         <label>Gender</label>
                         <div className="multiple-input-container">
-                        <input 
-                            id="man-gender-identity"
-                            type="radio"
-                            name="gender_identity"
-                            value="man"
-                            onChange={handleChange}
-                            checked={false}
-                        />
-                        <label htmlFor="man-gender-identity">Man</label>                   
-                        <input 
-                            id="woman-gender-identity"
-                            type="radio"
-                            name="gender_identity"
-                            value="woman"
-                            onChange={handleChange}
-                            checked={false}
-                        />
-                        <label htmlFor="woman-gender-identity">Woman</label>
-                        <input 
-                            id="nonbinary-gender-identity"
-                            type="radio"
-                            name="gender_identity"
-                            value="nonbinary"
-                            onChange={handleChange}
-                            checked={false}
-                        />
-                        <label htmlFor="nonbinary-gender-identity">Nonbinary</label>
-                        <input 
-                            id="more-gender-identity"
-                            type="radio"
-                            name="gender_identity"
-                            value="more"
-                            onChange={handleChange}
-                            checked={false}
-                        />
-                        <label htmlFor="more-gender-identity">More</label>
+                            <input 
+                                id="man-gender-identity"
+                                type="radio"
+                                name="gender_identity"
+                                value="man"
+                                onChange={handleChange}
+                                checked={false}
+                            />
+                            <label htmlFor="man-gender-identity">Man</label>                   
+                            <input 
+                                id="woman-gender-identity"
+                                type="radio"
+                                name="gender_identity"
+                                value="woman"
+                                onChange={handleChange}
+                                checked={false}
+                            />
+                            <label htmlFor="woman-gender-identity">Woman</label>
+                            <input 
+                                id="nonbinary-gender-identity"
+                                type="radio"
+                                name="gender_identity"
+                                value="nonbinary"
+                                onChange={handleChange}
+                                checked={false}
+                            />
+                            <label htmlFor="nonbinary-gender-identity">Nonbinary</label>
+                            <input 
+                                id="more-gender-identity"
+                                type="radio"
+                                name="gender_identity"
+                                value="more"
+                                onChange={handleChange}
+                                checked={false}
+                            />
+                            <label htmlFor="more-gender-identity">More</label>
                         </div>
 
                         <label htmlFor="show-gender">Show gender on my profile</label>
@@ -182,7 +211,7 @@ const Onboarding = () => {
                             name="about"
                             required={true}
                             placeholder="Experimental pop artist..."
-                            value={""}
+                            value={formData.about}
                             onChange={handleChange}
                         />
                         <label htmlFor="about">Profile Photo</label>
@@ -194,7 +223,7 @@ const Onboarding = () => {
                                 required={true}
                             />
                             <div className="photo-container">
-
+                                <img src={formData.url} alt="profile pic preview" />
 
                             </div>
 
